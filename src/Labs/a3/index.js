d9f0e13f213f71d8a474f0ac4e50f22ad28b42cc
ktmn713 <116754776+ktmn713@users.noreply.github.com>
2023-11-01 15:13:22 -0400

    a4 State and Redux fa23

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -3,10 +3,10 @@ import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
-import TodoItem from "./todos/TodoItem";
-import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
 
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
     return (
       <div className="container">
         <h1>Assignment 3</h1>
@@ -15,8 +15,13 @@ function Assignment3() {
         <Classes/>
         <Styles/>
         <ConditionalOutput/>
-        <TodoItem/>
-        <TodoList/>
+        <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,13 +1,31 @@
-import Assignment3 from "./a3";
 import Nav from "../Nav";
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
+import HelloRedux from "./a4/ReduxExamples/HelloRedux";
+import CounterRedux from "./a4/ReduxExamples/CounterRedux";
+import AddRedux from "./a4/ReduxExamples/AddRedux";
+import TodoList from "./a4/ReduxExamples/todos/TodoList";
 
 function Labs() {
   return (
-    <div className="container">
-      <Nav />
-      <h1>Assignment 3</h1>
-      <Assignment3 />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="a3" />} />
+          <Route path="a3" element={<Assignment3 />} />
+          <Route path="a4" element={<Assignment4 />} />
+        </Routes>
+        <HelloRedux />
+        <CounterRedux />
+        <AddRedux />
+        <TodoList />
+      </div>
+    </Provider>
   );
 }
+
 export default Labs;
